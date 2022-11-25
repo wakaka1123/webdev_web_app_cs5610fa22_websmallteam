c44f856641d95e4ea49355831e66f1138ae180b4
wakaka1123 <overmarsdyf@163.com>
2022-11-25 15:38:43 -0800

    changed url at omdb-search

--- src/omdb/omdb-service.js
+++ src/omdb/omdb-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SEARCH_URL = 'http://www.omdbapi.com/?apikey=a710cb0b&s='
+const SEARCH_URL = 'https://www.omdbapi.com/?apikey=a710cb0b&s='
 
 export const findMovieBySearchTerm = async (term) =>{
   const response = await axios.get(`${SEARCH_URL}${term}`)
