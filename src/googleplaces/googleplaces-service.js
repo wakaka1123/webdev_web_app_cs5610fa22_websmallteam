aaa819b753174074dc77f11846e1f79aed26f560
Lingfei Zhang <lingfeiz@Lingfeis-MacBook-Pro.local>
2022-12-09 13:48:56 -0800

    delete unused stuff

--- src/googleplaces/googleplaces-service.js
+++ src/googleplaces/googleplaces-service.js
@@ -1,14 +1,5 @@
 import axios from "axios";
 
-const SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json\n'
-    + '  ?fields=all\n'
-    + '  &inputtype=textquery\n'
-    + '  &key=AIzaSyC_VM4TqUh9-YOh9Off2mIf2i38Ui5PJYk\n'
-    + '  &input='
-const DETAILS_URL = 'https://maps.googleapis.com/maps/api/place/details/json\n'
-    + '  ?fields=all\n'
-    + '  &key=AIzaSyC_VM4TqUh9-YOh9Off2mIf2i38Ui5PJYk\n'
-    + '  &place_id='
 const PLACE_API_URL = 'http://localhost:4000/place'
 
 export const findPlaceBySearchTerm = async (name) => {
